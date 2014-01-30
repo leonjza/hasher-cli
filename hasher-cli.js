1077ca8029dc514a6836181870b38c6f523612e4
Leon Jacobs <leonja511@gmail.com>
2014-01-30 14:27:05 +0200

    Initial commit

---
+++ hasher-cli.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+var SHA3 = require('crypto-js/sha3')
+   Crypto = require('crypto-js')
+   read = require('read');
+
+// If we dont get a identifier, display a usage message
+if (process.argv.length <= 2) {
+   console.log('Usage: hasher.js <identifier> <output-length | optional>');
+   process.exit(1);
+}
+
+// Parse the console args
+var identifier = process.argv[2];
+if (typeof process.argv[3] !== 'undefined') { 
+   var length = process.argv[3];
+   var intRegex = /^\d+$/;
+   if(!intRegex.test(length)) {
+      console.log('I am not an int');
+      process.exit(1);
+   } else {
+      if (length > 32) {
+         console.log('The max output length is 32.');
+         var length = 32;
+      }
+   }
+} else {
+   var length = 32;
+}
+
+read({ prompt: 'Master Key: ', silent: true }, function(er, master_key) {
+   if (master_key.length <= 0) {
+      console.log('Error, the Master Key can not be empty.');
+      console.log('Usage: hasher.js <identifier> <output-length>');
+   } else {
+      var result = SHA3(master_key + identifier, { outputLength: 128 });
+      if (length != 32) {
+         console.log(result.toString(Crypto.enc.Hex).slice(0, -Math.abs(32 - length)));
+      } else {
+         console.log(result.toString(Crypto.enc.Hex));
+      }
+   }
+})
